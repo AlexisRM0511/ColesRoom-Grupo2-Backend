8b594617a16587c3eeb4eb4ad1ea4150ada710dd
AlexisRM0511 <alexis.rojas1@unmsm.edu.pe>
2021-07-18 23:53:33 -0500

    task

---
+++ routes/TasksRoute.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt'); //Usar despues para encriptar passwords
+const { response } = require('express');
+const usersModel = require('../models/Tasks.js');
+
+router.post('/taskcreate', async (req, res) => {
+    const { email, password } = req.body;
+    const userExists = await userModel.findOne({ email })
+    if (!userExists) {
+        res.status(404).json({ error: 'auth/user-not-found' })
+    }
+    else {
+        const correctPass = await userModel.findOne({ password })
+        if (!correctPass) {
+            res.status(404).json({ error: 'auth/wrong-password' })
+        }
+        else {
+            const idUser = correctPass._id
+            res.json({
+                status: 'ok',
+                id: idUser
+            })
+        }
+    }
+});
+
+router.post('/register', async (req, res) => {
+    console.log(req.body)
+    const { name, surname, email, password } = req.body;
+    
+    const emailExists = await userModel.findOne({ email })
+    if (emailExists) {
+        res.status(404).json({ error: 'auth/email-already-exists' })
+    }
+    else {
+        const user = new usersModel({
+            name,
+            email,
+            surname,
+            password
+        })
+        let idUser = ''
+        await user.save().then(u => idUser = u._id);        
+        
+        res.json({ status: 'ok', id: idUser })       
+    }
+
+
+    // user.save()
+    //     .then(data => {
+    //         res.json(data)
+    //     })
+    //     .catch(err => {
+    //         res.json(err)
+    //     })
+
+});
+
+module.exports = router;
