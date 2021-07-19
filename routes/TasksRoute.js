74b0625e7fc4ee5b0eae3b0fee1a46c4d22d9802
AlexisRM0511 <alexis.rojas1@unmsm.edu.pe>
2021-07-19 10:49:02 -0500

    cambios

--- routes/TasksRoute.js
+++ routes/TasksRoute.js
@@ -2,58 +2,36 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt'); //Usar despues para encriptar passwords
 const { response } = require('express');
-const usersModel = require('../models/Tasks.js');
+const tasksModel = require('../models/Tasks');
 
-router.post('/taskcreate', async (req, res) => {
-    const { email, password } = req.body;
-    const userExists = await userModel.findOne({ email })
-    if (!userExists) {
-        res.status(404).json({ error: 'auth/user-not-found' })
-    }
-    else {
-        const correctPass = await userModel.findOne({ password })
-        if (!correctPass) {
-            res.status(404).json({ error: 'auth/wrong-password' })
-        }
-        else {
-            const idUser = correctPass._id
-            res.json({
-                status: 'ok',
-                id: idUser
-            })
-        }
-    }
+router.post('/api/tasks', async (req, res) => {
+    const { user_id, publication_id, content, route } = req.body;
+    const task = new tasksModel({
+        user_id, publication_id, content, route  
+    });
+    
+    await task.save()
+
+    res.json({ status: 'ok'})
 });
 
-router.post('/register', async (req, res) => {
-    console.log(req.body)
-    const { name, surname, email, password } = req.body;
-    
-    const emailExists = await userModel.findOne({ email })
-    if (emailExists) {
-        res.status(404).json({ error: 'auth/email-already-exists' })
-    }
-    else {
-        const user = new usersModel({
-            name,
-            email,
-            surname,
-            password
-        })
-        let idUser = ''
-        await user.save().then(u => idUser = u._id);        
-        
-        res.json({ status: 'ok', id: idUser })       
-    }
+router.post('/editTasks', async (req, res) => {
+    const { user_id, publication_id, content, route,_id } = req.body;
 
+    const user = await tasksModel.updateOne({_id: _id}, {
+           $set: {
+                 name: '',
+                 email: '',
+                 password: ''
+               }
+             }) 
+});
 
-    // user.save()
-    //     .then(data => {
-    //         res.json(data)
-    //     })
-    //     .catch(err => {
-    //         res.json(err)
-    //     })
+router.post('/deleteTasks', async (req, res) => {
+    const  {_id} = req.body;
+    const user = await tasksModel.deleteOne({_id: id})
+    console.log(user)
+
 
 });
 
