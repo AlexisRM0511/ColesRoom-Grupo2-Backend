7a2a1d3ed1748e5a0789152410fa5e214c3183eb
AlexisRM0511 <alexis.rojas1@unmsm.edu.pe>
2021-07-26 02:06:09 -0500

    cambios

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/colesroomdb'
-mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true})
-    .then(db => console.log('La Base de Datos esta conectada'))
+const url = 'mongodb+srv://prueba_prueba:prueba_prueba@clustercolesroom.owdjh.mongodb.net/test'
+mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:true,useCreateIndex:true})
+    .then(db => console.log('La Base de Datos esta conectada: '))
     .catch(error => console.error(error));
 
 module.exports=mongoose;
