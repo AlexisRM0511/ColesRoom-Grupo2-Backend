fbe886faab57ba6e48e1c440bd3cf5be759aa85f
AlexisRM0511 <alexis.rojas1@unmsm.edu.pe>
2021-07-28 23:06:31 -0500

    v

--- routes/CourseRoute.js
+++ routes/CourseRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 //imports Models
 const coursesModel = require('../models/Course.js')
+const userModel = require('../models/User.js')
 
 //Get All Courses
 router.get('/api/courses', async (req, res) => {
@@ -14,13 +15,26 @@ router.get('/api/courses', async (req, res) => {
 router.get('/api/courses/:id', async (req, res) => {
     const course = await coursesModel.findById(req.params.id);
     res.json(course);
-  });
+});
 
 //CREATE Course  
 router.post('/api/CreateCourse', async (req, res) => {
     const { name, category, description, user_id } = req.body;
     const course = new coursesModel({ name, category, description, user_id })
-    await course.save().then(u => course_id = u._id)
+    let newCoursesCreated
+    let courseID
+    await course.save().then(async c => courseID=c._id)
+    await userModel.findById(course.user_id).then(u => {
+        newCoursesCreated = u.coursecreated
+        newCoursesCreated.push(courseID)
+        console.log(newCoursesCreated+", "+courseID)
+    })
+    console.log(course.user_id)
+    await userModel.findByIdAndUpdate( course.user_id, {
+        $set: {
+            coursecreated:newCoursesCreated
+        }
+    })
     res.json({ status: 'Curso Creado!' })
 });
 
@@ -40,24 +54,3 @@ router.delete('/api/courses/:id', async (req, res) => {
 
 
 module.exports = router;
-
-/*const teacher_id = course.user_id
-    userModel.updateOne({ _id: teacher_id }, {
-        $set: {
-            coursescreated: [course_id]
-        }
-    }, function (error, info) {
-        if (error) {
-            res.json({
-                resultado: false,
-                msg: 'No se pudo modificar el cliente',
-                err
-            });
-        } else {
-            res.json({
-                resultado: true,
-                info: info,
-
-            })
-        }
-    })*/
